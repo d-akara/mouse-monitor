3abee40455ebcb552a6626883a211cd785e4cdc5
Dakara <chad.meadows@gmail.com>
2019-03-19 22:37:07 -0400

    measure update hz

--- mouse.go
+++ mouse.go
@@ -2,29 +2,56 @@ package main
 
 import (
 	"fmt"
-	"syscall"
 	"time"
 	"unsafe"
 
-	"./winapi"
+	"github.com/dakaraphi/mouse-monitor/winapi"
 
 	"github.com/lxn/win"
 )
 
 type MouseData struct {
-	xPos         int32
-	yPos         int32
-	msgCount     int32
-	previousTime time.Time
+	xPos           int32
+	yPos           int32
+	msgCount       int32
+	msgHz          float64
+	msgHzLastTime  time.Time
+	msgHzLastCount int32
 }
 
+var displaySignal chan bool
+var messages chan string
 var mouseTracking MouseData
 
-func WndProc(hWnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+func handleMouseInput(mouseInput winapi.RAWMOUSE) {
+	mouseTracking.xPos += mouseInput.LastX
+	mouseTracking.yPos += mouseInput.LastY
+	mouseTracking.msgCount++
+
+	if mouseInput.ButtonData == 1 {
+		fmt.Printf("logged current position: x %v  y %v \r\n", mouseTracking.xPos, mouseTracking.yPos)
+		mouseTracking.xPos = 0
+		mouseTracking.yPos = 0
+		mouseTracking.msgCount = 0
+		mouseTracking.msgHzLastCount = 0
+		mouseTracking.msgHz = 0
+	} else if mouseTracking.msgCount > mouseTracking.msgHzLastCount+100 {
+		currentTime := time.Now()
+
+		currentHz := float64(1) / (currentTime.Sub(mouseTracking.msgHzLastTime).Seconds() / 100)
+		if currentHz > mouseTracking.msgHz {
+			mouseTracking.msgHz = currentHz
+		}
+		mouseTracking.msgHzLastTime = currentTime
+		mouseTracking.msgHzLastCount = mouseTracking.msgCount
+	}
+}
+
+func windowsMessageReceiver(hWnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	switch msg {
 	case win.WM_CREATE:
 		fmt.Println("Registering raw input mouse")
-		devices := getRawInputDevices(hWnd)
+		devices := winapi.GetRawInputDeviceMouseDefinition(hWnd)
 		len := uint32(len(devices))
 		size := uint32(unsafe.Sizeof(devices[0]))
 		if !win.RegisterRawInputDevices(&devices[0], len, size) {
@@ -36,20 +63,7 @@ func WndProc(hWnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 		var raw winapi.RAWINPUT
 		cbSize := uint32(unsafe.Sizeof(raw))
 		win.GetRawInputData((win.HRAWINPUT)(unsafe.Pointer(uintptr(lParam))), win.RID_INPUT, unsafe.Pointer(&raw), &cbSize, uint32(unsafe.Sizeof(winapi.RAWINPUTHEADER{})))
-		mouseTracking.xPos += raw.Mouse.LastX
-		mouseTracking.yPos += raw.Mouse.LastY
-		if raw.Mouse.ButtonData == 1 {
-			fmt.Printf("logged current position: x %v  y %v \r\n", mouseTracking.xPos, mouseTracking.yPos)
-			mouseTracking.xPos = 0
-			mouseTracking.yPos = 0
-			mouseTracking.msgCount = 0
-		} else {
-			mouseTracking.msgCount++
-			currentTime := time.Now()
-			diffTime := float64(1) / currentTime.Sub(mouseTracking.previousTime).Seconds()
-			fmt.Printf("mouse moved: x %v  y %v   msg %1.f    \r", mouseTracking.xPos, mouseTracking.yPos, diffTime)
-			mouseTracking.previousTime = currentTime
-		}
+		handleMouseInput(raw.Mouse)
 
 	case win.WM_DESTROY:
 		fmt.Println("destroying window")
@@ -61,42 +75,18 @@ func WndProc(hWnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	return 0
 }
 
-func getRawInputDevices(hWnd win.HWND) []win.RAWINPUTDEVICE {
-	devices := make([]win.RAWINPUTDEVICE, 1)
-	devices[0].UsUsagePage = 0x01
-	devices[0].UsUsage = 0x02
-	devices[0].DwFlags = win.RIDEV_INPUTSINK
-	devices[0].HwndTarget = hWnd
-	return devices
-}
-
-/* create window so we can receive messages from message loop */
-func createMessageLoop() int {
-	hInstance := win.GetModuleHandle(syscall.StringToUTF16Ptr(""))
-	lpszClassName := syscall.StringToUTF16Ptr("WNDclass")
-	var wcex win.WNDCLASSEX
-	wcex.HInstance = hInstance
-	wcex.LpszClassName = lpszClassName
-	wcex.LpfnWndProc = syscall.NewCallback(WndProc)
-	wcex.CbSize = uint32(unsafe.Sizeof(wcex))
-	win.RegisterClassEx(&wcex)
-	win.CreateWindowEx(
-		0, lpszClassName, syscall.StringToUTF16Ptr("Message Receiver Window"),
-		win.WS_OVERLAPPEDWINDOW,
-		win.CW_USEDEFAULT, win.CW_USEDEFAULT, 400, 400, 0, 0, hInstance, nil)
-	var msg win.MSG
+func display() {
 	for {
-		if win.GetMessage(&msg, 0, 0, 0) == 0 {
-			break
-		}
-		win.TranslateMessage(&msg)
-		win.DispatchMessage(&msg)
+		<-time.After(time.Millisecond * 50)
+		fmt.Printf("mouse moved: x %v  y %v   max update Hz %1.f    \r", mouseTracking.xPos, mouseTracking.yPos, mouseTracking.msgHz)
 	}
-	return int(msg.WParam)
 }
 
 func main() {
-	mouseTracking.previousTime = time.Now()
-	createMessageLoop()
+	displaySignal = make(chan bool)
+	mouseTracking.msgHzLastTime = time.Now()
+	go winapi.StartWindowsMessageLoop(windowsMessageReceiver)
+	display()
+
 	return
 }
--- winapi/winapi.go
+++ winapi/winapi.go
@@ -1,5 +1,14 @@
 package winapi
 
+import (
+	"syscall"
+	"unsafe"
+
+	"github.com/lxn/win"
+)
+
+type WndProc func(hWnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr
+
 // reference - https://docs.microsoft.com/en-us/windows/desktop/api/winuser/ns-winuser-tagrawmouse
 type RAWINPUTHEADER struct {
 	Type   uint32
@@ -23,4 +32,38 @@ type RAWMOUSE struct {
 	LastX            int32
 	LastY            int32
 	ExtraInformation uint32
+}
+
+/* create window so we can receive messages from message loop */
+func StartWindowsMessageLoop(windowsMessageReceiver WndProc) int {
+	hInstance := win.GetModuleHandle(syscall.StringToUTF16Ptr(""))
+	lpszClassName := syscall.StringToUTF16Ptr("WNDclass")
+	var wcex win.WNDCLASSEX
+	wcex.HInstance = hInstance
+	wcex.LpszClassName = lpszClassName
+	wcex.LpfnWndProc = syscall.NewCallback(windowsMessageReceiver)
+	wcex.CbSize = uint32(unsafe.Sizeof(wcex))
+	win.RegisterClassEx(&wcex)
+	win.CreateWindowEx(
+		0, lpszClassName, syscall.StringToUTF16Ptr("Message Receiver Window"),
+		win.WS_OVERLAPPEDWINDOW,
+		win.CW_USEDEFAULT, win.CW_USEDEFAULT, 400, 400, 0, 0, hInstance, nil)
+	var msg win.MSG
+	for {
+		if win.GetMessage(&msg, 0, 0, 0) == 0 {
+			break
+		}
+		win.TranslateMessage(&msg)
+		win.DispatchMessage(&msg)
+	}
+	return int(msg.WParam)
+}
+
+func GetRawInputDeviceMouseDefinition(hWnd win.HWND) []win.RAWINPUTDEVICE {
+	devices := make([]win.RAWINPUTDEVICE, 1)
+	devices[0].UsUsagePage = 0x01
+	devices[0].UsUsage = 0x02
+	devices[0].DwFlags = win.RIDEV_INPUTSINK
+	devices[0].HwndTarget = hWnd
+	return devices
 }
