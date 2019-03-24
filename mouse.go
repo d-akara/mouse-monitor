2ba5466aa8fb87bf4efb6a1a2e12939e3e916057
Dakara <chad.meadows@gmail.com>
2019-03-24 12:36:51 -0400

    add colors to console

--- mouse.go
+++ mouse.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/dakaraphi/mouse-monitor/winapi"
+	"github.com/fatih/color"
 )
+
+const messagesPerFrequencyCalculation = 100
 
 type MouseData struct {
 	xPos           int32
 	yPos           int32
-	msgCount       int32
+	msgCount       int64
 	msgHz          float64
 	msgHzLastTime  time.Time
-	msgHzLastCount int32
+	msgHzLastCount int64
 }
 
 var displaySignal chan bool
@@ -25,12 +29,13 @@ func handleMouseInput(mouseInput winapi.RAWMOUSE) {
 	mouseTracking.msgCount++
 
 	if mouseInput.ButtonData == 1 {
-		displaySignal <- true
+		displaySignal <- true // send display notification
+		<-displaySignal       // wait for display complete
 		mouseTracking = MouseData{}
-	} else if mouseTracking.msgCount > mouseTracking.msgHzLastCount+100 {
+	} else if mouseTracking.msgCount > mouseTracking.msgHzLastCount+messagesPerFrequencyCalculation {
 		currentTime := time.Now()
 
-		currentHz := float64(1) / (currentTime.Sub(mouseTracking.msgHzLastTime).Seconds() / 100)
+		currentHz := float64(1) / (currentTime.Sub(mouseTracking.msgHzLastTime).Seconds() / messagesPerFrequencyCalculation)
 		if currentHz > mouseTracking.msgHz {
 			mouseTracking.msgHz = currentHz
 		}
@@ -39,13 +44,22 @@ func handleMouseInput(mouseInput winapi.RAWMOUSE) {
 	}
 }
 
-func displayLoop() {
+func consoleDisplayLoop() {
+	valuesToColorStrings := func() (string, string, string) {
+		x := color.GreenString(strconv.FormatInt(int64(mouseTracking.xPos), 10))
+		y := color.GreenString(strconv.FormatInt(int64(mouseTracking.yPos), 10))
+		hz := color.CyanString(strconv.FormatFloat(mouseTracking.msgHz, 'f', 2, 64))
+		return x, y, hz
+	}
 	for {
 		select {
 		case <-time.After(time.Millisecond * 50):
-			fmt.Printf("current position: x %v  y %v   max update Hz %1.f    \r", mouseTracking.xPos, mouseTracking.yPos, mouseTracking.msgHz)
+			x, y, hz := valuesToColorStrings()
+			fmt.Fprintf(color.Output, "current position: x %v  y %v   max update Hz %v    \r", x, y, hz)
 		case <-displaySignal:
-			fmt.Printf("position delta: x %v  y %v   max update Hz %1.f    \r\n", mouseTracking.xPos, mouseTracking.yPos, mouseTracking.msgHz)
+			x, y, hz := valuesToColorStrings()
+			fmt.Fprintf(color.Output, "position delta: x %v  y %v   max update Hz %v          \r\n", x, y, hz)
+			displaySignal <- true
 		}
 	}
 }
@@ -54,7 +68,7 @@ func main() {
 	displaySignal = make(chan bool)
 	mouseTracking.msgHzLastTime = time.Now()
 	go winapi.StartWindowsMessageLoop(winapi.MakeMouseRawInputReceiver(handleMouseInput))
-	displayLoop()
+	consoleDisplayLoop()
 
 	return
 }
